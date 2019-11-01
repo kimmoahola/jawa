e42298c24e99b2b433fbb373ae02018cfd9bae1a
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-11-01 09:39:50 +0200

    Updates

---
+++ precache-manifest.d794122007a3d9d96fe22137ca19d014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c43acdd74c3067c5393e98212a0541",
+    "url": "/jawa/index.html"
+  },
+  {
+    "revision": "35338b34063e882996bd",
+    "url": "/jawa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ee55c3be48e6a46eafd7",
+    "url": "/jawa/static/js/2.988c750d.chunk.js"
+  },
+  {
+    "revision": "35338b34063e882996bd",
+    "url": "/jawa/static/js/main.db5ff33f.chunk.js"
+  },
+  {
+    "revision": "6b933a95cabdb8bf8cc0",
+    "url": "/jawa/static/js/runtime-main.82e6275d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jawa/static/media/logo.25bf045c.svg"
+  }
+]);
