dbd2da793cb52a4b6bd88585b9a87e45a4ba98f6
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-11-08 10:29:38 +0200

    Updates

--- precache-manifest.381bcce258ca3407867184c621235588.js
+++ precache-manifest.381bcce258ca3407867184c621235588.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4a1b05744c32c6533c59028374cff50",
+    "revision": "97292fbce2c7a7c0d4c69f05bf7a7801",
     "url": "/jawa/index.html"
   },
   {
-    "revision": "86a532ee67901fcd6b44",
+    "revision": "9653792d3437a96afd7a",
     "url": "/jawa/static/css/main.e20410b5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/jawa/static/js/2.16ea98f9.chunk.js"
   },
   {
-    "revision": "86a532ee67901fcd6b44",
-    "url": "/jawa/static/js/main.c465d776.chunk.js"
+    "revision": "9653792d3437a96afd7a",
+    "url": "/jawa/static/js/main.c941c8a3.chunk.js"
   },
   {
     "revision": "6b933a95cabdb8bf8cc0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jawa/precache-manifest.07670068382454d09551136f31f431af.js"
+  "/jawa/precache-manifest.381bcce258ca3407867184c621235588.js"
 );
 
 self.addEventListener('message', (event) => {
