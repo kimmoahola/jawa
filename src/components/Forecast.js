d6f5a67df1935cbb0ae52b961a62e6b016701110
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-11-15 10:57:33 +0200

    Change background color based on temperature
    Closes #1

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,8 +4,52 @@ import { DateInfo } from "./DateInfo";
 import { INTERESTING_HOURS } from "../config";
 import { Time } from "./Time";
 import { addHours, isSameDay } from "date-fns";
+import { hslToRgb, pickHex } from "../utils";
 
-function filterByInterestingHours2({ items, startOfDay }) {
+function temperatureBackgroundColor({ temperature }) {
+  if (!Number.isFinite(temperature)) {
+    return undefined;
+  }
+
+  const minTemp = -10;
+  const maxTemp = 25;
+
+  const lighterColdRgb = hslToRgb(245 / 360, 0.8, 0.16);
+  const darkerColdRgb = hslToRgb(245 / 360, 0.85, 0.12);
+  const lighterWarmRgb = hslToRgb(340 / 360, 0.85, 0.16);
+  const darkerWarmRgb = hslToRgb(340 / 360, 0.85, 0.14);
+
+  const ratio = Math.min(
+    Math.max((1 / (maxTemp - minTemp)) * (temperature - minTemp), 0),
+    1
+  );
+
+  const rgbLighter = `rgb(${pickHex(
+    lighterWarmRgb,
+    lighterColdRgb,
+    ratio
+  ).join()})`;
+
+  const rgbDarker = `rgb(${pickHex(
+    darkerWarmRgb,
+    darkerColdRgb,
+    ratio
+  ).join()})`;
+
+  const result = {
+    backgroundImage: `repeating-linear-gradient(
+      45deg,
+      ${rgbLighter},
+      ${rgbLighter} 0.8em,
+      ${rgbDarker} 0.8em,
+      ${rgbDarker} 1.6em
+    )`
+  };
+
+  return result;
+}
+
+function filterByInterestingHours({ items, startOfDay }) {
   const interestingHours = [
     new Date(),
     addHours(new Date(), 1),
@@ -19,7 +63,7 @@ function filterByInterestingHours2({ items, startOfDay }) {
 }
 
 function ForOneDay({ dayData }) {
-  const filtered = filterByInterestingHours2({
+  const filtered = filterByInterestingHours({
     items: dayData["items"],
     startOfDay: dayData["startOfDay"]
   });
@@ -35,7 +79,15 @@ function ForOneDay({ dayData }) {
       </div>
       <div className="weather-container">
         {filtered.map((i, index) => (
-          <div key={index} className="weather-item">
+          <div
+            key={index}
+            className="weather-item"
+            style={{
+              ...temperatureBackgroundColor({
+                temperature: i["temperature"]
+              })
+            }}
+          >
             <Time ts={i["ts"]} />
 
             <div className="weather-values">
